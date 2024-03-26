c4b7f9d1fddd7b53d59644ac21e99429f0839ea2
Alexander <abeaumier@deloitte.com>
2024-03-26 16:42:35 -0700

    initial commit

--- src/app.js
+++ src/app.js
@@ -6,11 +6,74 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.get('/musicians', async (req, res, next) => {
+        try {
+           const musicians = await Musician.findAll(req.params);
+           if (!musicians) {
+            throw new Error('No musicians found');
+           }
+            res.json(musicians);
+        } 
+        catch (error) {
+            next(error);
+        }
+    });
+   
+app.get ('/musicians/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const musicians = await Musician.findByPk(id);
+        if (!musicians) {
+            throw new Error('Musician not found');
+        }
+        res.json(musicians);
+    } catch (error) {
+        next(error);
+    }
+}
+);
 
+app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
 
+app.post('/musicians', async (req, res, next) => {
+    try {
+        const musician = await Musician.create(req.body);
+        res.json(musician);
+    } catch (error) {
+        next(error);
+    }
+});
 
+app.put('/musicians/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const musician = await Musician.findByPk(id);
+        if (!musician) {
+            throw new Error('Musician not found');
+        }
+        await musician.update(req.body);
+        res.json(musician);
+    } catch (error) {
+        next(error);
+    }
+});
 
+app.delete('/musicians/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const musician = await Musician.findByPk(id);
+        if (!musician) {
+            throw new Error('Musician not found');
+        }
+        await musician.destroy();
+        res.json({ message: 'Musician deleted successfully' });
+    } catch (error) {
+        next(error);
+    }
+}
+);
 
 
 module.exports = app;
